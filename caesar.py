1e7c45ceedd637eaf54f20c9d0c645498e15a0b1
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-10-31 23:41:37 +0300

    added changes to the caesar cipher.
    So basically we need to implement A_Z, a_z and 0_9 within their given range hence the corresponding mod.

--- caesar.py
+++ caesar.py
@@ -4,14 +4,39 @@ try:
 	key_pos = int(input("Enter a key for the encoding : "))
 	
 	for i in message:
-
-		diff = ord(i) - ord('A')
-		
-		new_diff = (diff + key_pos) % 26
-		
-		new_pos = new_diff + ord('A')
 	
-		print(chr(new_pos), end="") 
+		if i.isupper(): 
+
+			diff = ord(i) - ord('A')
+		
+			new_diff = (diff + key_pos) % 26
+		
+			new_pos = new_diff + ord('A')
+	
+			 
+
+
+		elif i.islower():
+
+			diff = ord(i) - ord('a')
+		
+			new_diff = (diff + key_pos) % 26
+		
+			new_pos = new_diff + ord('a')
+
+		elif i.isdigit():
+			
+			diff = ord(i) - ord('0')
+		
+			new_diff = (diff + key_pos) % 10
+		
+			new_pos = new_diff + ord('0')
+
+		else:
+			
+
+	print(chr(new_pos), end="")
+
 except:
 
 	print("enter a number, will you!? 😐️")
